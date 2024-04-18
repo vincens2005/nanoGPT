ecc54b80eaf60232422c6dbb10ae7147474cf0e4
cukmekerb <cukmekerb@gmail.com>
2024-04-17 22:39:22 -0700

    middle english training params

--- config/train_middle_english.py
+++ config/train_middle_english.py
@@ -15,23 +15,23 @@ wandb_run_name = 'mini-gpt'
 
 dataset = 'middle_english'
 gradient_accumulation_steps = 1
-batch_size = 64
-block_size = 256 # context of up to 256 previous characters
+batch_size = 12
+block_size = 1024 # context of up to 256 previous characters
 
 # baby GPT model :)
-n_layer = 6
-n_head = 6
-n_embd = 384
+n_layer = 24
+n_head = 12
+n_embd = 768
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 5000
-lr_decay_iters = 5000 # make equal to max_iters usually
+max_iters = 600000
+lr_decay_iters = 600000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 100 # not super necessary potentially
 
 # on macbook also add
-# device = 'cpu'  # run on cpu only
+device = 'cpu'  # run on cpu only
 # compile = False # do not torch compile the model
--- sample.py
+++ sample.py
@@ -6,6 +6,7 @@ import pickle
 from contextlib import nullcontext
 import torch
 import tiktoken
+import time
 from model import GPTConfig, GPT
 
 # -----------------------------------------------------------------------------
@@ -16,7 +17,7 @@ num_samples = 10 # number of samples to draw
 max_new_tokens = 500 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
-seed = 1337
+seed = time.time()
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = False # use PyTorch 2.0 to compile the model to be faster
